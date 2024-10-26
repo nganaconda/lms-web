ab7c3c1ef4a2eb460b271da20c66df74e3e1e198
nganaconda <nikosgeorgiou6@gmail.com>
2024-10-27 00:52:59 +0300

    Improved the test analysis page - admin view.
    On branch main
    Changes to be committed:
    	modified:   LMS/core/__pycache__/views.cpython-38.pyc
    	modified:   LMS/core/templates/core/test_analysis.html
    	modified:   LMS/core/views.py

--- LMS/core/views.py
+++ LMS/core/views.py
@@ -672,6 +672,7 @@ def test_view(request, test_gid):
             question_data = {
                 'question_text': question.question,
                 'question_weight': get_test_question(str(test_gid).replace('-', ''), str(question.gid).replace('-', '')),
+                'question_type': question.answerType,
                 'attributes': []
             }
             original_question = Question.objects.get(question=question.question)
